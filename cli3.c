d35f55a6e5385d686ba08e795f4995ca8813d6ab
Roger Esteve <rogeresteve30@gmail.com>
2023-03-02 10:20:54 +0100

    base udp server

---
+++ cli3.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define BUFFSIZE 255
+
+void err_sys(char *mess) { perror(mess); exit(1); }
+
+int create_socket(char* ip, char* port);
+
+int send_message(int socket, char* msg);
+
+int get_center(int a, int b);
+
+int create_socket(char* ip, char* port) {
+  struct sockaddr_in echoserver;
+  int sock, result;
+
+  sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sock < 0) {
+    err_sys("Error socket");
+  }
+
+  /* Set information for sockaddr_in */
+  memset(&echoserver, 0, sizeof(echoserver));       /* Reset memory */
+  echoserver.sin_family = AF_INET;                  /* Internet/IP */
+  echoserver.sin_addr.s_addr = inet_addr(ip);  /* Server address */
+  echoserver.sin_port = htons(atoi(port));       /* Server port */
+
+  /* Try to have a connection with the server */
+  result = connect(sock, (struct sockaddr *) &echoserver, sizeof(echoserver));
+  if (result < 0) {
+    err_sys("Error connect");
+  }
+
+  return sock;
+}
+
+int send_message(int socket, char* msg) {
+  write(socket, msg, strlen(msg) + 1);
+  return 0;
+}
+
+int getcenter(int a, int b) {
+  return ((b - a) / 2 + a);
+}
+
+int main(int argc, char *argv[]) {
+
+  /* Check input arguments */
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s <ip_server> <word> <port>\n", argv[0]);
+    exit(1);
+  }
+
+  /* Try to create TCP socket */
+
+  int socket = create_socket(argv[1], argv[3]);
+
+  int max = 100;
+  int x = max / 2;
+  int lo = 0, hi = max;
+
+  int found = 0;
+
+  char input[5];
+  int response = -1;
+  int it = 0;
+
+  while(response != 0) {
+    it++;
+    // Enviamos numero
+    sprintf(input, "%d", x); // passar de string a int i guardar en "input"
+    send_message(socket, input);
+
+    // Respuesta
+    char buffer[BUFFSIZE];
+    read(socket, buffer, BUFFSIZE);
+
+    response = atoi(buffer);
+    //fprintf(stdout, "%d", response);
+
+    // calculate next number
+    if (response < 0) {
+      printf("Selected number in the range [%d-%d]: %d\n", lo,hi,x);
+      printf("your choice is bigger than number: moving down interval\n\n");
+      hi = x;
+      x = getcenter(lo, x);
+    } else if (response > 0) {
+      printf("Selected number in the range [%d-%d]: %d\n", lo, hi, x);
+      printf("your choice is lower than number: moving up interval\n\n");
+      lo = x;
+      x = getcenter(x, hi);
+    } else {
+      printf("%d, The number is CORRECT!\n", x);
+      printf("You have found the right number in %d iterations!\n", it);
+      break;
+    }
+  }
+
+  send_message(socket, "/disc");
+
+  /* Close socket */
+  close(socket);
+
+
+  exit(0);
+}
---
+++ ser3.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <time.h>
+
+#define MAXPENDING 5    /* Maximum number of simultaneous connections */
+#define BUFFSIZE 10      /* Size of message to be reeived */
+
+#define MAX_LINES 1000
+#define MAX_LINE_LENGTH 255
+
+void err_sys(char *mess) { perror(mess); exit(1); }
+
+int create_server_socket(char* port);
+
+int socket_listen(int socket, struct sockaddr_in echoclient);
+
+int create_server_socket(char* port) {
+  struct sockaddr_in echoserver;
+  int serversock, clientsock;
+  int result;
+
+  /* Create TCP socket */
+  serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (serversock < 0)
+  {
+    err_sys("Error socket");
+  }
+
+  /* Set information for sockaddr_in structure */
+  memset(&echoserver, 0, sizeof(echoserver));     /* Reset memory */
+  echoserver.sin_family = AF_INET;                /* Internet/IP */
+  echoserver.sin_addr.s_addr = htonl(INADDR_ANY); /* Any address */
+  echoserver.sin_port = htons(atoi(port));     /* Server port */
+
+  /* Bind socket */
+  result = bind(serversock, (struct sockaddr *)&echoserver, sizeof(echoserver));
+  if (result < 0)
+  {
+    err_sys("Error bind");
+  }
+
+  /* Listen socket */
+  result = listen(serversock, MAXPENDING);
+  if (result < 0)
+  {
+    err_sys("Error listen");
+  }
+
+  return serversock;
+}
+
+int socket_listen(int socket, struct sockaddr_in echoclient) {
+  unsigned int clientlen = sizeof(echoclient);
+
+  int clientsock = accept(socket, (struct sockaddr *)&echoclient, &clientlen);
+  if (clientsock < 0)
+  {
+    err_sys("Error accept");
+  }
+  char *ip = inet_ntoa(echoclient.sin_addr);
+  fprintf(stdout, "Client: %s\n", ip);
+  return clientsock;
+}
+
+int* read_file_and_create_array(char* filename, int* lines) {
+    FILE *fp;
+    char buffer[MAX_LINE_LENGTH];
+    int* line_lengths;
+    int num_lines = 0;
+
+    line_lengths = malloc(MAX_LINES * sizeof(int));
+
+    fp = fopen(filename, "r");
+
+    while (fgets(buffer, MAX_LINE_LENGTH, fp))
+    {
+      int line_length = strlen(buffer);
+      if (line_length > 0 && buffer[line_length - 1] == '\n')
+      {
+        line_length--;
+      }
+      line_lengths[num_lines++] = line_length;
+    }
+
+    fclose(fp);
+    *lines = num_lines;
+    return line_lengths;
+}
+
+  int main(int argc, char *argv[])
+  {
+    struct sockaddr_in echoclient;
+
+    /* Check input arguments */
+    if (argc != 2)
+    {
+      fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+      exit(1);
+    }
+
+    int num_lines = 0;
+    int *line_lengths = read_file_and_create_array("x.txt", &num_lines);
+
+    int serversock = create_server_socket(argv[1]);
+
+    /* As a server we are in an infinite loop, waiting forever */
+    while (1)
+    {
+
+      /* Wait for a connection from a client */
+      printf("Listening...\n");
+      int clientsock = socket_listen(serversock, echoclient);
+
+      char buffer[BUFFSIZE] = "";
+
+      srand(time(NULL));
+      int number = line_lengths[rand() % num_lines];
+      int it = 0;
+
+      while (strcmp(buffer, "/disc"))
+      {
+        read(clientsock, &buffer[0], BUFFSIZE);
+        it++;
+        int guess = atoi(buffer);
+        if (guess > number)
+        {
+          char *msg = "-1";
+          write(clientsock, msg, strlen(msg) + 1);
+        } else if (guess < number){
+        char *msg = "1";
+        write(clientsock, msg, strlen(msg) + 1);
+      } else {
+        char *msg = "0";
+        write(clientsock, msg, strlen(msg) + 1);
+      }
+    }
+
+    printf("The random number is %d\n", number);
+    printf("Client has found the right number in %d iterations\n", it-1);
+
+    /* Close socket */
+    close(clientsock);  
+  }
+}
